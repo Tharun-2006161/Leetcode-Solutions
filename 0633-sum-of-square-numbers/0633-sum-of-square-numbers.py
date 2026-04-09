fd6e75773d96be15fd0aca2d0dbe0b1fb6da3026
GOLTHI THARUN SRI KALKI MURTHY <23A91A05F5@aec.edu.in>
2026-04-09 22:21:29 +0530

    Time: 55 ms (84.42%), Space: 19.3 MB (92.36%) - LeetHub

---
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.py
@@ -0,0 +1,13 @@
+class Solution:
+    def judgeSquareSum(self, c: int) -> bool:
+        i=0
+        j=int(c**0.5)
+        while i<=j:
+            s=i*i + j*j
+            if s==c:
+                return True
+            elif s<c:
+                i+=1
+            else:
+                j-=1
+        return False
