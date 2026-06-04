0c50486324f19b0a3ef3e3756ffa3c03444b4e75
GOLTHI THARUN SRI KALKI MURTHY <23A91A05F5@aec.edu.in>
2026-06-04 13:55:45 +0530

    Time: 0 ms (100%), Space: 19.3 MB (61.36%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.py
@@ -0,0 +1,19 @@
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        i=0
+        j=0
+        k=len(nums)-1
+        while i<=k:
+            if nums[i]==1:
+                i+=1
+            elif nums[i]==0:
+                nums[i],nums[j]=nums[j],nums[i]
+                i+=1
+                j+=1
+            else:
+                nums[i],nums[k]=nums[k],nums[i]
+                k-=1
+            
