43c1213bd814ceee5f61050fb028fdf2d73525ba
GOLTHI THARUN SRI KALKI MURTHY <23A91A05F5@aec.edu.in>
2026-06-09 17:51:01 +0530

    Time: 78 ms (8.49%), Space: 33.8 MB (56.01%) - LeetHub

--- 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.py
+++ 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.py
@@ -1,13 +1,29 @@
 class Solution:
     def pivotArray(self, nums: List[int], pivot: int) -> List[int]:
-        p=[]
-        for i in range(len(nums)):
+        # p=[]
+        # for i in range(len(nums)):
+        #     if nums[i]<pivot:
+        #         p.append(nums[i])
+        # for i in range(len(nums)):
+        #     if nums[i]==pivot:
+        #         p.append(nums[i])
+        # for i in range(len(nums)):
+        #     if nums[i]>pivot:
+        #         p.append(nums[i])
+        # return p
+        p=[pivot]*len(nums)
+        i=0
+        j=len(nums)-1
+        l=0
+        r=len(nums)-1
+        while i<len(nums) and j>=0:
             if nums[i]<pivot:
-                p.append(nums[i])
-        for i in range(len(nums)):
-            if nums[i]==pivot:
-                p.append(nums[i])
-        for i in range(len(nums)):
-            if nums[i]>pivot:
-                p.append(nums[i])
+                p[l]=nums[i]
+                l+=1
+               
+            if nums[j]>pivot:
+                p[r]=nums[j]
+                r-=1
+            i+=1
+            j-=1
         return p
