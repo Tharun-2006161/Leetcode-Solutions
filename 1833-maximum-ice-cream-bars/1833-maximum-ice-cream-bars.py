0b66af5aa2af38cd5bf7624a25ef61046e34c532
GOLTHI THARUN SRI KALKI MURTHY <23A91A05F5@aec.edu.in>
2026-06-21 22:20:57 +0530

    Time: 51 ms (92.25%), Space: 30.9 MB (90.23%) - LeetHub

---
+++ 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.py
@@ -0,0 +1,18 @@
+class Solution:
+    def maxIceCream(self, costs: List[int], coins: int) -> int:
+        s=max(costs)
+        cs=[0]*(s+1)
+        for i in range(len(costs)):
+            cs[costs[i]-1]+=1
+        res=0
+        for i in range(len(cs)):
+            if cs[i]>0:
+                if coins <(i+1):
+                        break
+                if (coins//(i+1) >= cs[i]):
+                    coins=coins-(cs[i]*(i+1))
+                    res+=cs[i]
+                else:
+                    res+=(coins//(i+1))
+                    break
+        return res
