24255429ef47e6bf988cf7447d74adf1e5ee083b
GOLTHI THARUN SRI KALKI MURTHY <23A91A05F5@aec.edu.in>
2026-07-21 14:17:55 +0530

    Time: 0 ms (100%), Space: 19.2 MB (94.54%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -1,28 +1,26 @@
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
         
-        def prefix(a,b):
-            i = 0
-            res = ""
-            while i<len(a) and i<len(b):
-                if a[i] != b[i]:
-                    break
-                else:
-                    res += a[i]
-                i += 1
-            return res
-    
-        if strs[0] == "":
-            return ""
-        if len(strs) == 1:
-            return strs[0]
-        p = prefix(strs[0],strs[1])
-
-        if len(strs) == 2:
-            return p
-        else:
-            for i in range(2,len(strs)):
-                p = prefix(p,strs[i])
-            return p
-
+        # def prefix(a,b):
+        #     i = 0
+        #     res = ""
+        #     while i<len(a) and i<len(b):
+        #         if a[i] != b[i]:
+        #             break
+        #         else:
+        #             res += a[i]
+        #         i += 1
+        #     return res
         
+        # if len(strs) < 2:
+        #     return strs[0]
+        # p = prefix(strs[0],strs[1])
+        # for i in range(2,len(strs)):
+        #     p = prefix(p,strs[i])
+        # return p
+
+        for i in range(len(strs[0])):
+            for j in range(1,len(strs)):
+                if i == len(strs[j]) or strs[j][i] != strs[0][i]:
+                    return strs[0][:i]
+        return strs[0]
