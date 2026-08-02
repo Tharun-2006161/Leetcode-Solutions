11b8e8adbdb3b2433ec30321c740ce3de8c5b923
GOLTHI THARUN SRI KALKI MURTHY <23A91A05F5@aec.edu.in>
2026-08-02 15:31:20 +0530

    Time: 107 ms (90.68%), Space: 19.3 MB (67.26%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.py
+++ 0647-palindromic-substrings/0647-palindromic-substrings.py
@@ -1,12 +1,19 @@
 class Solution:
     def countSubstrings(self, s: str) -> int:
-        c=0
-        n=len(s)
-        for i in range(2*n-1):
-            l=i//2
-            r=(i+1)//2
-            while l>=0 and r<n and s[l]==s[r]:
-                c+=1
-                l-=1
-                r+=1
+        # c = 0
+        # for i in range(len(s)):
+        #     for j in range(i + 1,len(s)):
+        #         p = s[i : j + 1]
+        #         if p == p[::-1]:
+        #             c += 1
+        # print(c + len(s))
+        c = 0
+        n = len(s)
+        for i in range(2 * n - 1):
+            l = i // 2
+            r = (i + 1) // 2
+            while l >= 0 and r < n and s[l] == s[r]:
+                c += 1
+                l -= 1
+                r += 1
         return c
