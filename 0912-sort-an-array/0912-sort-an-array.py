ef4763b169936ee71f0986946388fee7d112418d
GOLTHI THARUN SRI KALKI MURTHY <23A91A05F5@aec.edu.in>
2026-08-03 15:40:03 +0530

    Time: 691 ms (34.13%), Space: 28.4 MB (30.25%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.py
+++ 0912-sort-an-array/0912-sort-an-array.py
@@ -1,32 +1,30 @@
 class Solution:
     def sortArray(self, nums: List[int]) -> List[int]:
-        def merge(nums):
-            n=len(nums)
-            if n<=1:
-                return
-            mid=n//2
-            left_arr = nums[:mid]
-            right_arr=nums[mid:]
-            merge(left_arr)
-            merge(right_arr)
-            i=0
-            j=0
-            k=0
-            while i<len(left_arr) and j<len(right_arr):
-                if left_arr[i]>right_arr[j]:
-                    nums[k]=right_arr[j]
-                    j+=1
+        
+        def merge_sort(nums):
+            if len(nums) <= 1:
+                return nums
+            mid = len(nums) // 2
+            left_arr = merge_sort(nums[:mid])
+            right_arr = merge_sort(nums[mid:])
+            return merge(left_arr,right_arr)
+        def merge(left_arr,right_arr):
+            i = j = 0
+            res = []
+            while i < len(left_arr) and j < len(right_arr):
+                if left_arr[i] < right_arr[j]:
+                    res.append(left_arr[i])
+                    i += 1
                 else:
-                    nums[k]=left_arr[i]
-                    i+=1
-                k+=1
-            while i<len(left_arr):
-                nums[k]=left_arr[i]
-                i+=1
-                k+=1
-            while j<len(right_arr):
-                nums[k]=right_arr[j]
-                j+=1
-                k+=1
-        merge(nums)
-        return nums
+                    res.append(right_arr[j])
+                    j += 1
+            while i < len(left_arr):
+                res.append(left_arr[i])
+                i += 1
+            while j < len(right_arr):
+                res.append(right_arr[j])
+                j += 1
+
+            return res
+        
+        return merge_sort(nums)
