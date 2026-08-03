f58743849f228b53341faf60ad0a05756db6c484
GOLTHI THARUN SRI KALKI MURTHY <23A91A05F5@aec.edu.in>
2026-08-03 14:08:05 +0530

    Time: 6 ms (56.92%), Space: 23 MB (41.69%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,12 +1,13 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        f={}
+
+        mpp = {}
         for i in range(len(nums)):
-            f[nums[i]]=f.get(nums[i],0)+1
-        a=sorted(f.items(),key=lambda x:-x[1])
-        print(a)
-        t=[]
-        for i in a:
-            t.append(i[0])
-        return (t[:k])
-            
+            mpp[nums[i]] = mpp.get(nums[i], 0) + 1
+        # st = sorted(mpp.items(), key = lambda x:(-x[1],x[0]))
+        st = sorted(mpp.items(), key = lambda x:x[1], reverse = True)
+        res1 = []
+        for i in range(k):
+            res1.append(st[i][0])
+
+        return res1      
