5cae9c85eca9c9ffd25470c65693a32cd43173c5
GOLTHI THARUN SRI KALKI MURTHY <23A91A05F5@aec.edu.in>
2026-08-06 20:35:48 +0530

    Time: 0 ms (100%), Space: 19.4 MB (38.18%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -3,8 +3,9 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        nums1[:]=nums1[:m]
-        for i in nums2:
-            nums1.append(i)
-        nums1[:] = sorted(nums1[:m+n])
-        return nums1
+        i = 0
+        while i < len(nums2) and m < len(nums1):
+            nums1[m], nums2[i] = nums2[i], nums1[m]
+            i += 1
+            m += 1
+        nums1.sort()
