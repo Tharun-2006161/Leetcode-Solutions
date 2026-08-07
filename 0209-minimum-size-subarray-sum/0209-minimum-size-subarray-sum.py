f4b0501eb06045249f24e35a2a084c3cf576d35e
GOLTHI THARUN SRI KALKI MURTHY <23A91A05F5@aec.edu.in>
2026-08-07 14:17:10 +0530

    Time: 46 ms (5.15%), Space: 30.7 MB (16.45%) - LeetHub

---
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.py
@@ -0,0 +1,21 @@
+class Solution:
+    def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+        
+        mi = 999999999
+        i = 0
+        j = 0
+        prefix = 0
+        while i < len(nums) and j < len(nums):
+            prefix += nums[i]
+            while prefix >= target:
+                mi = min(mi, i - j + 1)
+                print(mi)
+                prefix -= nums[j]
+                j += 1
+            i += 1
+        if mi != 999999999:
+            return mi
+        else:
+            return 0
+            
+            
