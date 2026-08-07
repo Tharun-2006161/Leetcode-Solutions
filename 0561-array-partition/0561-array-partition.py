a99fef1411cf2488deded94b1025aa97dd912aa2
GOLTHI THARUN SRI KALKI MURTHY <23A91A05F5@aec.edu.in>
2026-08-07 22:25:51 +0530

    Time: 15 ms (98.8%), Space: 21.5 MB (23.68%) - LeetHub

---
+++ 0561-array-partition/0561-array-partition.py
@@ -0,0 +1,11 @@
+class Solution:
+    def arrayPairSum(self, nums: List[int]) -> int:
+        nums.sort()
+        i = 0
+        j = 1
+        s = 0
+        while i < len(nums) and j < len(nums):
+            s += min(nums[i], nums[j])
+            i += 2
+            j += 2
+        return s
