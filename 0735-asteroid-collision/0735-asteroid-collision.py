7437e695ef666e72900717ac1eb6aec15b296f2b
GOLTHI THARUN SRI KALKI MURTHY <23A91A05F5@aec.edu.in>
2026-08-10 21:45:12 +0530

    Time: 7 ms (57.77%), Space: 20.3 MB (70.06%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.py
+++ 0735-asteroid-collision/0735-asteroid-collision.py
@@ -1,17 +1,15 @@
 class Solution:
     def asteroidCollision(self, asteroids: List[int]) -> List[int]:
-        a=asteroids
-        st=[]
-        for i in range(len(a)):
-            if a[i]>=0:
-                st.append(a[i])
-            elif a[i]<0 and st and st[-1]>=0:
-                while (st and st[-1]>0 and abs(a[i])>st[-1]):
-                    st.pop()
-                if st and abs(a[i]) == st[-1]:
-                    st.pop()
-                elif len(st)==0 or st[-1]<0:
-                    st.append(a[i])         
-            else:
-                st.append(a[i])
-        return st
+        res = []
+        for i in asteroids:
+            while res and i < 0 and res[-1] > 0:
+                if abs(i) > res[-1]:
+                    res.pop()
+                    continue
+                elif abs(i) == res[-1]:
+                    res.pop()
+                i = 0
+                break
+            if i != 0:
+                res.append(i)
+        return res
