1220107e62b467a8b266b859e2e78088f169a5ec
GOLTHI THARUN SRI KALKI MURTHY <23A91A05F5@aec.edu.in>
2026-08-11 23:07:33 +0530

    Time: 320 ms (5.02%), Space: 20.6 MB (42.39%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.py
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.py
@@ -4,22 +4,21 @@ class Solution:
         def binary(m):
             c = 0
             for i in range(len(piles)):
-                if m >= piles[i]:
+                if piles[i] <= m:
                     c += 1
-                elif m < piles[i]:
-                    c += ((piles[i] // m))
+                elif piles[i] > m:
+                    c += (piles[i] // m)
                     if piles[i] % m != 0:
                         c += 1
-
             return c <= h
-
-        i = 1
-        j = max(piles)
-        while i <= j:
-            m = (i + j) // 2
+        l = 1
+        r = max(piles)
+        
+        while l <= r:
+            m = (l + r) // 2
             if binary(m):
-                j = m - 1
+                r = m - 1
             else:
-                i = m + 1
-        return i
-
+                l = m + 1
+        return l
+        
