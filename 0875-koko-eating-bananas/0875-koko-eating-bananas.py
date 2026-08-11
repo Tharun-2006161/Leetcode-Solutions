e177cf9426734cb346ef86fbb3b867cd2f51b083
GOLTHI THARUN SRI KALKI MURTHY <23A91A05F5@aec.edu.in>
2026-08-11 11:59:13 +0530

    Time: 300 ms (5.02%), Space: 20.8 MB (11.81%) - LeetHub

---
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.py
@@ -0,0 +1,25 @@
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+
+        def binary(m,piles):
+            c = 0
+            for i in range(len(piles)):
+                if m >= piles[i]:
+                    c += 1
+                elif m < piles[i]:
+                    c += ((piles[i] // m))
+                    if piles[i] % m != 0:
+                        c += 1
+
+            return c <= h
+
+        i = 1
+        j = max(piles)
+        while i <= j:
+            m = (i + j) // 2
+            if binary(m,piles):
+                j = m - 1
+            else:
+                i = m + 1
+        return i
+
