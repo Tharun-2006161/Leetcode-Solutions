75c27deda571557b787d22b4cdfb36c79f4b1187
GOLTHI THARUN SRI KALKI MURTHY <23A91A05F5@aec.edu.in>
2026-08-15 20:28:38 +0530

    Time: 41 ms (33.06%), Space: 33 MB (88.71%) - LeetHub

--- 3702-longest-subsequence-with-non-zero-bitwise-xor/3702-longest-subsequence-with-non-zero-bitwise-xor.py
+++ 3702-longest-subsequence-with-non-zero-bitwise-xor/3702-longest-subsequence-with-non-zero-bitwise-xor.py
@@ -1,14 +1,13 @@
 class Solution:
     def longestSubsequence(self, nums: List[int]) -> int:
-        res = []
         prefix = 0
         for i in range(len(nums)):
             prefix = prefix ^ nums[i]
-            res.append(prefix)
-        print(res)
-        if res[-1] != 0:
+        
+        if prefix != 0:
             return len(nums)
-        for i in range(len(nums) - 1, -1, -1):
+        
+        for i in range(len(nums)):
             if nums[i] != 0:
                 return len(nums) - 1
         return 0
