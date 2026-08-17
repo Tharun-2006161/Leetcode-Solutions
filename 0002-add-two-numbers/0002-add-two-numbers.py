794c1b5233ae28ac8f15552716f395007badcc84
GOLTHI THARUN SRI KALKI MURTHY <23A91A05F5@aec.edu.in>
2026-08-17 11:53:57 +0530

    Time: 0 ms (100%), Space: 19.2 MB (78.29%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -5,38 +5,37 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        f=l1
-        s=l2
-        m=ListNode()
-        temp=m
-        carry=0
-        while f is not None and s is not None:
-            p=f.val+s.val+carry
-            digits=p%10
-            carry=p//10
+
+        lst1 = l1
+        lst2 = l2
+        dummy = ListNode()
+        temp = dummy
+        carry = 0
+        while lst1 != None and lst2 != None:
+            s = lst1.val + lst2.val + carry
+            digits = s % 10
+            carry = s // 10
             newnode = ListNode(digits)
-            temp.next=newnode
-            temp=temp.next
-            f=f.next
-            s=s.next
-        while f is not None:
-            p=f.val+carry
-            digits=p%10
-            carry=p//10
-            newnode=ListNode(digits)
-            temp.next=newnode
-            temp=temp.next
-            f=f.next
-        while s is not None:
-            p=s.val+carry
-            digits=p%10
-            carry=p//10
-            newnode=ListNode(digits)
-            temp.next=newnode
-            temp=temp.next
-            s=s.next
-        if carry>0:
-            li=ListNode(carry)
-            temp.next=li
-        return m.next
-        
+            temp.next = newnode
+            temp = temp.next
+            lst1 = lst1.next
+            lst2 = lst2.next
+        while lst1 != None:
+            s = lst1.val + carry
+            digits = s % 10
+            carry = s // 10
+            newnode = ListNode(digits)
+            temp.next = newnode
+            temp = temp.next
+            lst1 = lst1.next
+        while lst2 != None:
+            s = lst2.val + carry
+            digits = s % 10
+            carry = s // 10
+            newnode = ListNode(digits)
+            temp.next = newnode
+            temp = temp.next
+            lst2 = lst2.next
+        if carry > 0:
+            temp.next = ListNode(carry)
+        return dummy.next
