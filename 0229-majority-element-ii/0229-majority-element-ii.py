0283dba743c297ac20ff1ac688858e2e86ebe07d
GOLTHI THARUN SRI KALKI MURTHY <23A91A05F5@aec.edu.in>
2026-08-17 21:54:35 +0530

    Time: 6 ms (71.05%), Space: 22.5 MB (62.62%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.py
+++ 0229-majority-element-ii/0229-majority-element-ii.py
@@ -2,13 +2,35 @@ class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
 
         n = len(nums)
-        res = {}
-        
-        for i in range(n):
-            res[nums[i]] = res.get(nums[i], 0) + 1
-
-        res1 = []
-        for i,j in res.items():
-            if j > n // 3:
-                res1.append(i)
-        return res1
+        first = None
+        second = None
+        c1 = 0
+        c2 = 0
+        for i in nums:
+            if i == first:
+                c1 += 1
+            elif i == second:
+                c2 += 1
+            elif c1 == 0:
+                first = i
+                c1 += 1
+            elif c2 == 0:
+                second = i
+                c2 += 1
+            else:
+                c1 -= 1
+                c2 -= 1
+        ele1 = 0
+        ele2 = 0
+        for i in nums:
+            if i == first:
+                ele1 += 1
+            elif i == second:
+                ele2 += 1
+        res = []
+        if ele1 > n // 3:
+            res.append(first)
+        if ele2 > n // 3:
+            res.append(second)
+        return res
+    
