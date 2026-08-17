934f9f7ae26f706a0ba9163bfa48ff9e63e3e5ae
GOLTHI THARUN SRI KALKI MURTHY <23A91A05F5@aec.edu.in>
2026-08-17 10:48:42 +0530

    Time: 825 ms (5.73%), Space: 22.6 MB (54.69%) - LeetHub

---
+++ 0141-linked-list-cycle/0141-linked-list-cycle.py
@@ -0,0 +1,17 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        arr = []
+        temp = head
+        while temp != None:
+            if temp in arr:
+                return True
+            arr.append(temp)
+            temp = temp.next
+        return False
+            
