cef442599719e6f5d5d148ed172c8cb9f4ad0e46
GOLTHI THARUN SRI KALKI MURTHY <23A91A05F5@aec.edu.in>
2026-08-21 21:14:17 +0530

    Time: 162 ms (61.45%), Space: 41.4 MB (73.99%) - LeetHub

--- 0853-car-fleet/0853-car-fleet.py
+++ 0853-car-fleet/0853-car-fleet.py
@@ -1,18 +1,9 @@
 class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
-        res = []
-        for i in range(len(position)):
-            res.append((position[i],((target - position[i]) / speed[i])))
-        res1 = sorted(res,key = lambda x : x[0],reverse = True)
-        res2 = []
-        for i in range(len(res1)):
-            res2.append(res1[i][1])
-        c = 1
-        prev = res2[0]
-        for i in range(1,len(res2)):
-            if res2[i] <= prev:
-                continue
-            else:
-                c += 1
-                prev = res2[i]
-        return c
+        s = sorted(zip(position,speed),reverse = True)
+        st = []
+        for i,j in s:
+            p = (target - i) / j
+            if not st or p > st[-1]:
+                st.append(p)
+        return len(st)
