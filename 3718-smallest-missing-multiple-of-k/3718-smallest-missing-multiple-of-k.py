96b9f07063b057f5852151c2941aebf9daec6dbe
GOLTHI THARUN SRI KALKI MURTHY <23A91A05F5@aec.edu.in>
2026-08-25 06:35:00 +0530

    Time: 0 ms (100%), Space: 19.2 MB (88.59%) - LeetHub

---
+++ 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.py
@@ -0,0 +1,10 @@
+class Solution:
+    def missingMultiple(self, nums: List[int], k: int) -> int:
+        s = set(nums)
+        i = 1
+        while True:
+            if i * k not in s:
+                return i * k
+            else:
+                i += 1
+        
