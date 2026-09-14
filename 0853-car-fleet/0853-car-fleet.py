36a400fc944f2911eafc911bab1355088d9a985f
GOLTHI THARUN SRI KALKI MURTHY <23A91A05F5@aec.edu.in>
2026-09-14 22:55:04 +0530

    Time: 231 ms (25.13%), Space: 42.7 MB (17.97%) - LeetHub

--- 0853-car-fleet/0853-car-fleet.py
+++ 0853-car-fleet/0853-car-fleet.py
@@ -1,6 +1,7 @@
 class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
         s = sorted(zip(position,speed),reverse = True)
+        print(s)
         st = []
         for i,j in s:
             p = (target - i) / j
